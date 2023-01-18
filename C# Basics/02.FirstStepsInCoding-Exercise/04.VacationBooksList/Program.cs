b13e72f0fdcb68d166500bb0bccbebdbcfc6b320
Petar Bogdanov <petarbogdanov0412@gmail.com>
2023-01-18 10:14:34 +0200

    Added First Steps In Coding - Lab and -Exercise

---
+++ C# Basics/02.FirstStepsInCoding-Exercise/04.VacationBooksList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.VacationBooksList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPages = int.Parse(Console.ReadLine());
+
+            int pages = int.Parse(Console.ReadLine());
+
+            int numberOfDays = int.Parse(Console.ReadLine());
+
+            int hoursToRead = numberOfPages / pages / numberOfDays;
+
+            Console.WriteLine(hoursToRead);
+        }
+    }
+}
---
+++ C# Basics/02.FirstStepsInCoding-Exercise/05.SuppliesForSchool/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05.SuppliesForSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double packetOfPens = 5.8;
+
+            double packetOfMarkers = 7.2;
+
+            double cleanerPerLitre = 1.2;
+
+            int numberOfPacketsOfPens = int.Parse(Console.ReadLine());
+
+            int numberOfPacketsOfMarkers = int.Parse(Console.ReadLine());
+
+            int cleanerLitres = int.Parse(Console.ReadLine());
+
+            double discountPercent = double.Parse(Console.ReadLine()) / 100;
+
+            double totalPacketsOfPens = numberOfPacketsOfPens * packetOfPens;
+
+            double totalPacketsOfMarkers = numberOfPacketsOfMarkers * packetOfMarkers;
+
+            double totalCleanerLitres = cleanerLitres * cleanerPerLitre;
+
+            double sum = totalPacketsOfPens + totalPacketsOfMarkers + totalCleanerLitres;
+
+            double sumWithDiscount = sum - (sum * discountPercent);
+
+            Console.WriteLine(sumWithDiscount);
+        }
+    }
+}
